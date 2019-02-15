d5e5681d9e710998fef60c44f45b7613525f8fff
Simon Grützner <sg@math.uni-bremen.de>
2019-02-15 20:44:48 +0100

    ObjIdent with pre-trained VGG16 and 2 additional FC top-layer

--- main_script.py
+++ main_script.py
@@ -10,14 +10,11 @@ from keras.layers import Flatten, Dense, Activation, Dropout
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator, image
 
-# import tensorflow as tf
-# from keras.callbacks import TensorBoard
-
 # Setting parameters
 dir_train = 'dogscats/train'
 dir_val = 'dogscats/valid'
-batch_size = 64
-epochs = 2
+batch_size = 16
+epochs = 10
 
 # Building the Model
 vgg_model = applications.VGG16(weights='imagenet', include_top=False, input_shape=(150, 150, 3))
@@ -25,7 +22,7 @@ model = Sequential()
 
 # Setting Base Layers to 'untrainable'
 for layer in vgg_model.layers:
-    layer.trainable=False
+    layer.trainable = False
     model.add(layer)
 
 # Adding Top Layers
@@ -53,8 +50,8 @@ test_data_gen = ImageDataGenerator(rescale=1./255)
 
 train_set = train_data_gen.flow_from_directory(
     dir_train,
-    target_size=(150,150),
-    batch_size= batch_size,
+    target_size=(150, 150),
+    batch_size=batch_size,
     class_mode='binary'
 )
 test_set = test_data_gen.flow_from_directory(
@@ -66,21 +63,24 @@ test_set = test_data_gen.flow_from_directory(
 
 print(model.summary())
 
-tic=time.time()
+# tic
+tic = time.time()
 
 history = model.fit_generator(
     train_set,
     steps_per_epoch=2000 // batch_size,
     epochs=epochs,
     validation_data=test_set,
-    validation_steps= 800 // batch_size
+    validation_steps=800 // batch_size
 )
 
-toc=time.time()
-print('Computation Time is: ' + str((toc-tic) // pow(60,2))
-      + 'std ' + str(((toc-tic) % pow(60,2)) // 60)
-      + 'min ' + str(((toc-tic) % pow(60,2)) % 60)
-      + 'sec'
+# toc
+toc = time.time()
+
+# Computation time is
+print('Computation Time is: ' + str((toc-tic) // pow(60, 2)) + ':'
+      + str(((toc-tic) % pow(60, 2)) // 60) + ':'
+      + str(((toc-tic) % pow(60, 2)) % 60) + ':'
 )
 
 model.save_weights('2nd_try.h5')
@@ -93,7 +93,7 @@ plt.show()
 test_image = image.load_img(image_path, target_size=(150, 150))
 
 test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
+test_image = np.expand_dims(test_image, axis=0)
 result = model.predict(test_image)
 
 if result >= 0.5:
@@ -110,5 +110,5 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.ylim(0, 1)
-plt.grid(axis= 'both')
+plt.grid(axis='both')
 plt.show()
